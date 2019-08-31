f5669175e76216839d84563ff214ef6b82aacb81
Renan Nobre Garcia <renan.nobregarcia@gmail.com>
2019-08-31 14:11:21 -0300

    POST /projects route

--- src/server.js
+++ src/server.js
@@ -9,6 +9,26 @@ const projects = []
 server.get('/projects', (req, res) => {
   res.json(projects)
 })
+
+server.post('/projects', (req, res) => {
+  const { id, title, tasks } = req.body
+
+  if (
+    typeof id !== 'string' ||
+    typeof title !== 'string' ||
+    !Array.isArray(tasks)
+  ) {
+    return res.status(400).json({ error: 'Invalid data types' })
+  }
+
+  if (projects.find(project => project.id === id)) {
+    return res.status(400).json({ error: 'Project already exists' })
+  }
+
+  projects.push({ id, title, tasks })
+
+  return res.json({ message: 'Project was included successfully' })
+})
 })
 
 server.listen(3000)
